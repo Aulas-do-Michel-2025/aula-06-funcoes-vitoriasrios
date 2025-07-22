4c7cd1e1a99e08f86da27c77a7b02c7f9369c733
vitoriasrios <vitoriasrios97@gmail.com>
2025-07-22 20:04:11 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -10,3 +10,18 @@ Se a nota for < 3 -> retorne "Reprovado".
 Obs: Escreva necessariamente uma função que retorne (não só imprima) esses valores. De exatamente o nome de `verificar_nota` porque
 o teste vai procurar uma função com esse nome.
 """
+
+
+
+nota = float(input("Qual é a nota:"))
+vefificar_nota = ""
+
+def verificar_nota(nota):
+
+    if nota > 5:
+        return "Aprovado"
+    elif 3 <= nota <= 5:
+        return "Recuperação"
+    else:
+        return "Reprovado"
+print(verificar_nota(nota))
